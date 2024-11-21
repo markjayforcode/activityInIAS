b260378c97b9c01df612e4a6c55dbd77070051aa
markjayforcode <markjayforcode@yahoo.com>
2024-11-21 04:32:00 -0800

    Updated the homepage. Adjusted font-style and nav bg

---
+++ phpFiles/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "blogsite";
+
+//create connection
+$conn = new  mysqli($servername,$username,$password,$db);
+
+//check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
